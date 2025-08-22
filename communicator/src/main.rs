af18c399b10e8e7c47d5e8e700cb31e98a5d9145
OldMartijntje_ubuntu <oldmartijntje@gmail.com>
2025-08-22 20:56:30 +0200

    incursion test

--- communicator/src/main.rs
+++ communicator/src/main.rs
@@ -1,3 +1,12 @@
+use std::process::Command;
+
 fn main() {
-    println!("Okay boomer");
+    let mut cmd = Command::new("cargo");
+    cmd.arg("run");
+
+    let hello_1 = cmd.spawn().expect("failed to spawn").wait_with_output().expect("failed output");
+    let hello_2 = cmd.spawn().expect("failed to spawn").wait_with_output().expect("failed output");
+
+    println!("First run: {}", String::from_utf8_lossy(&hello_1.stdout));
+    println!("Second run: {}", String::from_utf8_lossy(&hello_2.stdout));
 }
