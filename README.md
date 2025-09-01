e5e88a30ade77ac630ecd5c24c146b0b7e1392dc
oldmartijntje_TH <oldmartijntje@gmail.com>
2025-09-01 16:33:32 +0200

    6:58

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cautious-fishstick
-Trying to teach myself rust
+This project is me learning rust.
+
+It is a ffmpeg wrapper.
+
+I followed [this tutorial](https://www.youtube.com/watch?v=72PyU1EIGY8) for the Iced UI tutorial.
--- cautious-fishstick/src/main.rs
+++ cautious-fishstick/src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use ::iced::theme::Theme;
+use iced::{Alignment, Sandbox, Settings};
+
+pub fn main() -> iced::Result {
+    RustUI::run(Settings::default())
+}
+
+struct RustUI {
+    theme: Theme,
+    page: Page,
+    login_field: LoginField
+}
+
+struct LoginField {email: String, password: String}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Page {Login, Register}
+
+#[derive(Debug, Clone)]
+enum Message {
+    ToggleTheme,
+    LoginSubmit,
+    Router(String), // change page depending on route
+    LoginFIeldCHanged(String, String),
+}
+
+impl Sandbox for RustUI {
+    type Message = Message;
+    fn new() -> Self {
+        Self {
+            theme: Theme::Dark, // default start theme
+            page: Page::Login, // default start page
+            login_field: LoginField {
+                email: String::new(),
+                password: String::new()
+            },
+        }
+    }
+
+    // the app title
+    fn title (&self) -> String {
+        String::from("Cautious Fishstick - Iced Rust UI")
+    }
+
+    fn theme(&self) -> Theme {
+        self.theme.clone() // return a copy of the theme.
+    }
 }
